c6075b5207a1551fe41aeece6a3d731be2aa0859
KATARINA\katarina <katarina1234@gmail.com>
2023-05-16 18:56:17 +0200

    path changed for imgs sport

--- TestingWebSocketDisplayService2222/wwwroot/js/sports-list.js
+++ TestingWebSocketDisplayService2222/wwwroot/js/sports-list.js
@@ -171,32 +171,32 @@ document.addEventListener("DOMContentLoaded", function () {
 function getImagePath(sport) {
     switch (sport) {
         case "ru":
-            return "~/Icons/rugby-union.png";
+            return "Icons/rugby-union.png";
         case "rl":
-            return "~/Icons/rugby-league.png";
+            return "Icons/rugby-league.png";
         case "baseball":
-            return "~/Icons/baseball.png";
+            return "Icons/baseball.png";
         case "af":
-            return "~/Icons/american-football.png";
+            return "Icons/american-football.png";
         case "basket":
-            return "~/Icons/basketball.png";
+            return "Icons/basketball.png";
         case "basket_ht":
-            return "~/Icons/basketball_ht.jpg";
+            return "Icons/basketball_ht.jpg";
         case "boxing":
-            return "~/Icons/boxing-gloves.png";
+            return "Icons/boxing-gloves.png";
         case "cricket":
-            return "~/Icons/cricket.png";
+            return "Icons/cricket.png";
         case "fb":
         case "fb_ht":
         case "fb_et":
         case "fb_corn":
         case "fb_corn_ht":
         case "fb_htft":
-            return "~/Icons/football-game.png";
+            return "Icons/football-game.png";
         case "ih":
-            return "~/Icons/hockey.png";
+            return "Icons/hockey.png";
         case "tennis":
-            return "~/Icons/tennis.png";
+            return "Icons/tennis.png";
         default:
             return "";
     }
