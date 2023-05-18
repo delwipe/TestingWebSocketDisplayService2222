2154e9c59913709a99d131cae34b387160bca0ed
KATARINA\katarina <katarina1234@gmail.com>
2023-05-18 18:52:54 +0200

    deleted

--- TestingWebSocketDisplayService2222/wwwroot/js/sports-list.js
+++ TestingWebSocketDisplayService2222/wwwroot/js/sports-list.js
@@ -77,37 +77,37 @@
 //}
 
 
-function getImagePath(sport) {
-    switch (sport) {
-        case "ru":
-            return "Icons/rugby-union.png";
-        case "rl":
-            return "Icons/rugby-league.png";
-        case "baseball":
-            return "Icons/baseball.png";
-        case "af":
-            return "Icons/american-football.png";
-        case "basket":
-            return "Icons/basketball.png";
-        case "basket_ht":
-            return "Icons/basketball_ht.jpg";
-        case "boxing":
-            return "Icons/boxing-gloves.png";
-        case "cricket":
-            return "Icons/cricket.png";
-        case "fb":
-        case "fb_ht":
-        case "fb_et":
-        case "fb_corn":
-        case "fb_corn_ht":
-        case "fb_htft":
-            return "Icons/football-game.png";
-        case "ih":
-            return "Icons/hockey.png";
-        case "tennis":
-            return "Icons/tennis.png";
-        default:
-            return "";
-    }
-}
+//function getImagePath(sport) {
+//    switch (sport) {
+//        case "ru":
+//            return "Icons/rugby-union.png";
+//        case "rl":
+//            return "Icons/rugby-league.png";
+//        case "baseball":
+//            return "Icons/baseball.png";
+//        case "af":
+//            return "Icons/american-football.png";
+//        case "basket":
+//            return "Icons/basketball.png";
+//        case "basket_ht":
+//            return "Icons/basketball_ht.jpg";
+//        case "boxing":
+//            return "Icons/boxing-gloves.png";
+//        case "cricket":
+//            return "Icons/cricket.png";
+//        case "fb":
+//        case "fb_ht":
+//        case "fb_et":
+//        case "fb_corn":
+//        case "fb_corn_ht":
+//        case "fb_htft":
+//            return "Icons/football-game.png";
+//        case "ih":
+//            return "Icons/hockey.png";
+//        case "tennis":
+//            return "Icons/tennis.png";
+//        default:
+//            return "";
+//    }
+//}
 
